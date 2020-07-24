eed9d52d4e4a90740b429b1b0187ef27f55d2607
prahladmadhav <prahladmadhav@gmail.com>
2020-07-24 19:44:37 +0530

    (#002) Working with Modules

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log("Hello World")
+const tutorial = require("./tutorial");
+console.log(tutorial);
+console.log(tutorial.sum(1,1));
+console.log(tutorial.PI);
+console.log(new tutorial.SomeClass());
