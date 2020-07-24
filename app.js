1cfb4df1127f933d263a8d19426ac8704c42f5a9
prahladmadhav <prahladmadhav@gmail.com>
2020-07-24 20:07:06 +0530

    (#005) EventEmmiter- on() & emit() with extends class

--- app.js
+++ app.js
@@ -6,3 +6,21 @@ eventEmitter.on("tutorial",(num1,num2)=>{
 })
 
 eventEmitter.emit("tutorial",1,2);
+
+class Person extends EventEmitter{
+    constructor(name){
+        super();
+        this._name = name;
+    }
+
+    get name(){
+        return this._name;
+    }
+}
+
+let madhav = new Person('Madhav');
+madhav.on('name',()=>{
+    console.log('My name is '+ madhav.name);
+})
+
+madhav.emit('name');
