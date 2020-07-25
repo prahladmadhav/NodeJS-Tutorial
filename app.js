b7d05a40587130dfd11a169399c915906d51e904
prahladmadhav <prahladmadhav@gmail.com>
2020-07-25 13:16:40 +0530

    (#006) Working With The ReadLine Module

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
-const EventEmitter = require("events");
-const eventEmitter = new EventEmitter();
+const readline = require('readline');
+const rl = readline.createInterface({input : process.stdin,
+                                    output : process.stdout});
 
-eventEmitter.on("tutorial",(num1,num2)=>{
-    console.log(num1+num2);
-})
+let num1 = Math.floor((Math.random() * 10) + 1)
+let num2 = Math.floor((Math.random() * 10) + 1)
+let answer = num1 + num2
 
-eventEmitter.emit("tutorial",1,2);
-
-class Person extends EventEmitter{
-    constructor(name){
-        super();
-        this._name = name;
+rl.question(`(Q) What is ${num1} + ${num2}? \n(A) `,
+(userInput)=>{
+    if (userInput.trim() == answer){
+        rl.close();
     }
-
-    get name(){
-        return this._name;
+    else{
+        rl.setPrompt(`Incorrect! Try again. \n(A) `);
+        rl.prompt();
+        rl.on('line',(userInput)=>{
+            if(userInput.trim() == answer){
+                rl.close();
+            }
+            else{
+                rl.setPrompt(`Your answer ${userInput} is incorrect! Try again. \n(A) `);
+                rl.prompt();
+            }
+        })
     }
-}
+});
 
-let madhav = new Person('Madhav');
-madhav.on('name',()=>{
-    console.log('My name is '+ madhav.name);
+rl.on('close',()=>{
+    console.log(`Correct`);
 })
-
-madhav.emit('name');
