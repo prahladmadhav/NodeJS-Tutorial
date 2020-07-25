4c1ea04678a540d6e49634e3afebc5f7d57829c6
prahladmadhav <prahladmadhav@gmail.com>
2020-07-25 20:18:29 +0530

    (#010) Creating a Http Server using the Http Module

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const fs = require('fs');
-const zlib = require('zlib');
-const gzip = zlib.createGzip();
-const gunzip = zlib.createGunzip();
-const readStream = fs.createReadStream('./example.txt','utf8');
-const writeStream = fs.createWriteStream('./example2.txt.gz')
-
-// readStream.pipe(writeStream);
-
-// readStream.pipe(gzip).pipe(writeStream);
-
-//readStream.pipe(gunzip).pipe(writeStream);
+const http = require('http');
+const fs =require('fs')
+const server = http.createServer((req,res)=>{
+    const readStream = fs.createReadStream('./static/example.html');
+    res.writeHead(200,{'Content-type' : 'text/html'});
+    readStream.pipe(res);
+    // const readStream = fs.createReadStream('./static/example.json');
+    // res.writeHead(200,{'Content-type' : 'application/json'});
+    // readStream.pipe(res);
+    // const readStream = fs.createReadStream('./static/example.png');
+    // res.writeHead(200,{'Content-type' : 'image/png'});
+    // readStream.pipe(res);
+}).listen(3000);
