ffd1c38525a7b2ba58f17830bb7ea659b91d3102
prahladmadhav <prahladmadhav@gmail.com>
2020-07-26 14:21:10 +0530

    (#011) Create our Package.json using Npm Init | Installing Packages using Npm (Node Package Manager) | Semantic Versioning

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-const http = require('http');
-const fs =require('fs')
-const server = http.createServer((req,res)=>{
-    const readStream = fs.createReadStream('./static/example.html');
-    res.writeHead(200,{'Content-type' : 'text/html'});
-    readStream.pipe(res);
-    // const readStream = fs.createReadStream('./static/example.json');
-    // res.writeHead(200,{'Content-type' : 'application/json'});
-    // readStream.pipe(res);
-    // const readStream = fs.createReadStream('./static/example.png');
-    // res.writeHead(200,{'Content-type' : 'image/png'});
-    // readStream.pipe(res);
-}).listen(3000);
+const _ = require('lodash');
+let example = _.fill([1,2,3,4,5],'banana',1,4);
+console.log(example);
